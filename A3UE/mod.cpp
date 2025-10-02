77f1dd018b1dd5626fc1b576b91569723e8aa370
Connor Walsh <connor.w.misc@gmail.com>
2025-10-02 02:30:10 -0400

    fixing up

--- A3UE/mod.cpp
+++ A3UE/mod.cpp
@@ -1,5 +1,4 @@
-name = "A3 Antistasi Ultimate Extender example";
-author = "A3-Antistasi dev team, A3-Antistasi Ultimate dev team";
-tooltip = "Antistasi Ultimate Extender example";
-actionName = "GitHub";
-action = "https://github.com/Westalgie/A3UExtender"
+name = "A3UE - The Yulakian War For Independence [v0.6.0]";
+author = "* Black Ops *";
+actionName = "Website";
+action = "https://github.com/Black-Ops-2-543/A3UE_Yulakia_Extender";
